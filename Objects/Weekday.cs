005c254d0669a1ccbcc882c40d254da9c46305d4
Derek Villars, Lawrence Eby and grinil khanna <derek+lawrence+grinil@>
2017-02-16 14:03:23 -0800

    Add functionality for leap year testing spec

--- Objects/Weekday.cs
+++ Objects/Weekday.cs
@@ -6,18 +6,82 @@ namespace WeekdayCalc
     public class Weekday
     {
         private string _date;
-        private int _parsedYear;
-
+        private int _parsedFullYear;
+        private int _parsedLastTwoYear;
+        private int _parsedDay;
+        private int _parsedMonth;
+        private int _remainderYear;
+        private int _dayPlusYear;
+        private int _afterMonthValue;
+        public static Dictionary<int, int> monthKey = new Dictionary<int, int>()
+        {
+            {01, 1}, {02, 4}, {03, 4}, {04, 0}, {05, 2}, {06, 5}, {07, 0}, {08, 3}, {09, 6}, {10, 1}, {11, 4}, {12, 6}
+        };
         public Weekday(string date)
         {
             _date = date;
-            string spliceDate = _date.Substring(8);
-            _parsedYear = int.Parse(spliceDate);
+
+            string spliceFullYear = date.Substring(6,4);
+            _parsedFullYear = int.Parse(spliceFullYear);
+
+            string spliceHalfYear = date.Substring(8,2);
+            _parsedLastTwoYear = int.Parse(spliceHalfYear);
+
+            string spliceDay = date.Substring(3,2);
+            _parsedDay = int.Parse(spliceDay);
+
+            string spliceMonth = date.Substring(0,2);
+            _parsedMonth = int.Parse(spliceMonth);
+
+            _remainderYear = _parsedLastTwoYear % 4;
+            _dayPlusYear = _parsedLastTwoYear/4 + _parsedDay;
+            _afterMonthValue = _dayPlusYear + monthKey[_parsedMonth];
+        }
+
+        public void LeapYearTest()
+        {
+            if (_parsedFullYear % 400 == 0)
+            {
+                _afterMonthValue += 1;
+            }
+            else if(_parsedFullYear % 100 == 0)
+            {
+                _afterMonthValue += 0;
+            }
+            else if (_parsedFullYear % 4 == 0)
+            {
+                _afterMonthValue += 1;
+            }
+            else
+            {
+                _afterMonthValue += 0;
+            }
         }
 
         public int GetParsedYear()
         {
-            return _parsedYear;
+            return _parsedLastTwoYear;
+        }
+
+        public int GetDividedYear()
+        {
+            return _parsedLastTwoYear/4;
+        }
+
+        public int GetRemainder()
+        {
+            return _remainderYear;
+        }
+
+        public int GetDayPlusYear()
+        {
+            return _dayPlusYear;
+        }
+
+        public int GetAfterMonthValue()
+        {
+            LeapYearTest();
+            return _afterMonthValue;
         }
     }
 
--- Tests/WeekdayTest.cs
+++ Tests/WeekdayTest.cs
@@ -7,9 +7,8 @@ namespace WeekdayCalc
     public class WeekdayTest
     {
         [Fact]
-        public void Test1_LastTwoDigits_Output()
+        public void Test1_LastTwoDigits_17()
         {
-
             //Arrange
             string userInput = "02/16/2017";
             int testResult = 17;
@@ -21,8 +20,81 @@ namespace WeekdayCalc
             //Assert
                           //Arrange  //Act
             Assert.Equal(testResult, result);
+        }
 
+        [Fact]
+        public void Test2_DividebyFour_4()
+        {
+            //Arrange
+            string userInput = "02/16/2017";
+            int testResult = 4;
 
+            //Act
+            Weekday newWeekday = new Weekday(userInput);
+            int result = newWeekday.GetDividedYear();
+
+            //Assert
+            Assert.Equal(testResult, result);
+        }
+
+        [Fact]
+        public void Test3_RemainderTest_1()
+        {
+            //Arrange
+            string userInput = "02/16/2017";
+            int testResult = 1;
+
+            //Act
+            Weekday newWeekday = new Weekday(userInput);
+            int result = newWeekday.GetRemainder();
+
+            //Assert
+            Assert.Equal(testResult, result);
+        }
+
+        [Fact]
+        public void Test4_AddDaytoDividedYear_20()
+        {
+            //Arrange
+            string userInput = "02/16/2017";
+            int testResult = 20;
+
+            //Act
+            Weekday newWeekday = new Weekday(userInput);
+            int result = newWeekday.GetDayPlusYear();
+
+            //Assert
+            Assert.Equal(testResult, result);
+        }
+
+        [Fact]
+        public void Test5_AddMonthValuetoDayplusYear_24()
+        {
+            //Arrange
+            string userInput = "02/16/2017";
+            int testResult = 24;
+
+            //Act
+            Weekday newWeekday = new Weekday(userInput);
+            int result = newWeekday.GetAfterMonthValue();
+
+            //Assert
+            Assert.Equal(testResult, result);
+        }
+
+        [Fact]
+        public void Test6_LeapYearTest_24()
+        {
+            //Arrange
+            string userInput = "02/16/2017";
+            int testResult = 24;
+
+            //Act
+            Weekday newWeekday = new Weekday(userInput);
+            int result = newWeekday.GetAfterMonthValue();
+
+            //Assert
+            Assert.Equal(testResult, result);
         }
     }
 }
